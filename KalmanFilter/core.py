eb4c920368a14b2500f14a21264f159def414e51
Jiaxing <liujxing@gmail.com>
2019-09-01 13:33:14 -0400

    added matrix dimension check

--- KalmanFilter/core.py
+++ KalmanFilter/core.py
@@ -4,9 +4,57 @@ from typing import *
 
 class KalmanMatrix(object):
 
-    def __init__(self, state_transition_matrix=None, transition_noise_matrix=None,
+    def __init__(self, state_dim, observation_dim,
+                 state_transition_matrix=None, transition_noise_matrix=None,
                  observation_output_matrix=None, observation_noise_matrix=None,
                  initial_mean_matrix=None, initial_covariance_matrix=None):
+
+        # perform dimension check
+        if state_transition_matrix is not None:
+            if state_transition_matrix.ndim != 2:
+                raise ValueError("The dimension of state_transition_matrix should be 2 but get {}".format(state_transition_matrix.ndim))
+            if state_transition_matrix.shape[0] != state_dim or state_transition_matrix.shape[1] != state_dim:
+                raise ValueError("The shape of state_transition_matrix is {} but the state_dim is {}".format(state_transition_matrix.shape, state_dim))
+
+        if transition_noise_matrix is not None:
+            if transition_noise_matrix.ndim != 2:
+                raise ValueError("The dimension of transition_noise_matrix should be 2 but get {}".format(transition_noise_matrix.ndim))
+            if transition_noise_matrix.shape[0] != state_dim or transition_noise_matrix.shape[1] != state_dim:
+                raise ValueError("The shape of transition_noise_matrix is {} but the state_dim is {}".format(transition_noise_matrix.shape, state_dim))
+
+        if observation_output_matrix is not None:
+            if observation_output_matrix.ndim == 1 and observation_dim == 1:
+                observation_output_matrix = observation_output_matrix.reshape(1, -1)
+            if observation_output_matrix.ndim != 2:
+                raise ValueError("The dimension of observation_output_matrix should be 2 but get {}".format(observation_output_matrix.ndim))
+            if observation_output_matrix.shape[0] != observation_dim or observation_output_matrix.shape[1] != state_dim:
+                raise ValueError("The shape of observation_output_matrix is {} but expecting ({}, {})".format(observation_output_matrix.shape, observation_dim, state_dim))
+
+        if observation_noise_matrix is not None:
+            if observation_noise_matrix.ndim == 1:
+                observation_noise_matrix = observation_noise_matrix.reshape(-1, 1)
+            if observation_noise_matrix.ndim != 2:
+                raise ValueError("The dimension of observation_noise_matrix should be 2 but get {}".format(observation_noise_matrix.ndim))
+            if observation_noise_matrix.shape[0] != observation_dim or observation_noise_matrix.shape[1] != 1:
+                raise ValueError("The shape of observation_noise_matrix is {} but expecting ({}, 1)".format(observation_noise_matrix.shape, observation_dim))
+
+        if initial_mean_matrix is not None:
+            if initial_mean_matrix.ndim == 1:
+                initial_mean_matrix = initial_mean_matrix.reshape(-1, 1)
+            if initial_mean_matrix.ndim != 2:
+                raise ValueError("The dimension of initial_mean_matrix should be 2 but get {}".format(initial_mean_matrix.ndim))
+            if initial_mean_matrix.shape[0] != state_dim or initial_mean_matrix.shape[1] != 1:
+                raise ValueError("The shape of initial_mean_matrix is {} but expecting ({}, {})".format(initial_mean_matrix.shape, state_dim, 1))
+
+        if initial_covariance_matrix is not None:
+            if initial_covariance_matrix.ndim != 2:
+                raise ValueError("The dimension of initial_covariance_matrix should be 2 but get {}".format(initial_covariance_matrix.ndim))
+            if initial_covariance_matrix.shape[0] != state_dim or initial_covariance_matrix.shape[1] != state_dim:
+                raise ValueError("The shape of initial_covariance_matrix is {} but expecting ({}, {})".format(initial_covariance_matrix.shape, state_dim, state_dim))
+
+        # save the variables
+        self.state_dim = state_dim
+        self.observation_dim = observation_dim
 
         self.state_transition_matrix = state_transition_matrix
         self.transition_noise_matrix = transition_noise_matrix
@@ -16,12 +64,28 @@ class KalmanMatrix(object):
         self.initial_covariance_matrix = initial_covariance_matrix
 
     def get_state_dim(self):
-        return len(self.transition_noise_matrix)
+        return self.state_dim
 
     def get_observation_dim(self):
-        return len(self.observation_noise_matrix)
+        return self.observation_dim
 
-    # functions for forward filtering
+    def _copy_optional_matrix(self, matrix):
+        if matrix is None:
+            return None
+        else:
+            return matrix.copy()
+
+    def copy(self):
+        matrix_copy = KalmanMatrix(self.state_dim, self.observation_dim,
+                                   self._copy_optional_matrix(self.state_transition_matrix),
+                                   self._copy_optional_matrix(self.transition_noise_matrix),
+                                   self._copy_optional_matrix(self.observation_output_matrix),
+                                   self._copy_optional_matrix(self.observation_noise_matrix),
+                                   self._copy_optional_matrix(self.initial_mean_matrix),
+                                   self._copy_optional_matrix(self.initial_covariance_matrix))
+        return matrix_copy
+
+    ####################### functions for forward filtering #########################
 
     def get_initial_forward_mean(self) -> np.ndarray:
         return self.initial_mean_matrix
@@ -48,7 +112,7 @@ class KalmanMatrix(object):
     def get_posterior_mean(self, prior_mean, gain_matrix, observation):
         return prior_mean + gain_matrix @ (observation - self.observation_output_matrix @ prior_mean)
 
-    # functions for backward smoothing
+    ####################### functions for backward smoothing #######################
 
     def get_backward_intermediate_matrix(self, posterior_cov, next_prior_cov):
         return posterior_cov @ self.state_transition_matrix.T @ np.linalg.inv(next_prior_cov)
@@ -71,7 +135,7 @@ class KalmanMatrix(object):
     def get_initial_smooth_mean(self, next_smooth_mean, next_prior_mean, backward_intermediate_matrix):
         return self.get_smooth_mean(self.get_initial_forward_cov(), next_smooth_mean, next_prior_mean, backward_intermediate_matrix)
 
-    # functions for optimization
+    ######################## functions for optimization #######################
 
     def get_smooth_second_moment(self, smooth_mean, smooth_cov):
         smooth_mean = smooth_mean.reshape(-1, 1)
